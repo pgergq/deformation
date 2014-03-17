4eaa4a81c760833cf3617ca8fdbd0e591685db6e
pgergq <pgergq@gmail.com>
2014-03-17 16:54:49 +0100

    added per-vertex shading

--- Deformation/ParticleDraw.hlsl
+++ Deformation/ParticleDraw.hlsl
@@ -8,46 +8,45 @@
 // @Modified by pgq
 //--------------------------------------------------------------------------------------
 
+
 struct VSParticleIn
 {
-    float4  color   : COLOR;
-    uint    id      : SV_VertexID;
+	float4  color   : COLOR;
+	uint    id      : SV_VertexID;
 };
 
 struct VSParticleDrawOut
 {
-    float3 pos			: POSITION;
-    float3 normal		: TEXCOORD3;
-    float3 mpid1		: TEXCOORD1;
-    float3 mpid2		: TEXCOORD2;
-    float4 color		: COLOR;
+	float3 pos			: POSITION;
+	float3 npos			: TEXCOORD3;
+	float3 mpid1		: TEXCOORD1;
+	float3 mpid2		: TEXCOORD2;
+	float4 color		: COLOR;
 };
 
 struct GSParticleDrawOut
 {
-    float2 tex			: TEXCOORD0;
-    float3 normal		: TEXCOORD3;
-    float3 mpid1		: TEXCOORD1;
-    float3 mpid2		: TEXCOORD2;
-    float4 color		: COLOR;
-    float4 pos			: SV_Position;
+	float2 tex			: TEXCOORD0;
+	float3 mpid1		: TEXCOORD1;
+	float3 mpid2		: TEXCOORD2;
+	float4 color		: COLOR;
+	float4 pos			: SV_Position;
 };
 
 struct PSParticleDrawIn
 {
-    float2 tex			: TEXCOORD0;
-    float3 normal		: TEXCOORD3;
-    float3 mpid1		: TEXCOORD1;
-    float3 mpid2		: TEXCOORD2;
-    float4 color		: COLOR;
+	float2 tex			: TEXCOORD0;
+	float3 mpid1		: TEXCOORD1;
+	float3 mpid2		: TEXCOORD2;
+	float4 color		: COLOR;
 };
 
 struct Particle
 {
-    float4 pos			: SV_Position;
-    float4 normal		: TEXCOORD3;
-    float4 mpid1		: TEXCOORD1;
-    float4 mpid2		: TEXCOORD2;
+	float4 pos			: SV_Position;
+	float4 npos			: TEXCOORD3;
+	float4 mpid1		: TEXCOORD1;
+	float4 mpid2		: TEXCOORD2;
 };
 
 Texture2D		            g_txDiffuse;
@@ -56,57 +55,70 @@ StructuredBuffer<Particle>   g_bufParticle;
 
 SamplerState g_samLinear
 {
-    Filter = MIN_MAG_MIP_LINEAR;
-    AddressU = Clamp;
-    AddressV = Clamp;
+	Filter = MIN_MAG_MIP_LINEAR;
+	AddressU = Clamp;
+	AddressV = Clamp;
 };
 
 cbuffer cb0
 {
-    row_major float4x4 g_mWorldViewProj;
-    row_major float4x4 g_mInvView;    
+	row_major float4x4 g_mWorldViewProj;
+	row_major float4x4 g_mInvView;
+	float4 eyepos;
+	float4 lightpos;
 };
 
 cbuffer cb1
 {
-    static float g_fParticleRad = 10.0f;   
+	static float g_fParticleRad = 10.0f;
 };
 
 cbuffer cbImmutable
 {
-    static float3 g_positions[4] =
-    {
-        float3( -1, 1, 0 ),
-        float3( 1, 1, 0 ),
-        float3( -1, -1, 0 ),
-        float3( 1, -1, 0 ),
-    };
-    
-    static float2 g_texcoords[4] = 
-    { 
-        float2(0,0), 
-        float2(1,0),
-        float2(0,1),
-        float2(1,1),
-    };
+	static float3 g_positions[4] =
+	{
+		float3(-1, 1, 0),
+		float3(1, 1, 0),
+		float3(-1, -1, 0),
+		float3(1, -1, 0),
+	};
+
+	static float2 g_texcoords[4] =
+	{
+		float2(0, 0),
+		float2(1, 0),
+		float2(0, 1),
+		float2(1, 1),
+	};
 };
+
+//
+// Phong-Blinn, ruby
+//
+float4 PhongBlinn(float3 n, float3 h, float3 l){
+	float4 ka = float4(0.1745, 0.01175, 0.01175, 0.55);
+	float4 kd = float4(0.61424, 0.04136, 0.04136, 0.55);
+	float4 ks = float4(0.727811, 0.626959, 0.626959, 0.55);
+	float shininess = 76.8;
+	return ka + kd * saturate(dot(n, l)) + ks * pow(saturate(dot(n, h)), shininess);
+}
 
 //
 // Vertex shader for drawing the point-sprite particles
 //
 VSParticleDrawOut VSParticleDraw(VSParticleIn input)
 {
-    VSParticleDrawOut output;
-    
-    output.pos = g_bufParticle[input.id].pos.xyz;
-    output.normal = g_bufParticle[input.id].normal.xyz;
-    output.mpid1 = g_bufParticle[input.id].mpid1.xyz;
-    output.mpid2 = g_bufParticle[input.id].mpid2.xyz;
+	VSParticleDrawOut output;
 
-    // load colour from Particle color
-    output.color = input.color;
-    
-    return output;
+	output.pos = g_bufParticle[input.id].pos.xyz;
+	output.npos = g_bufParticle[input.id].npos.xyz;
+	output.mpid1 = g_bufParticle[input.id].mpid1.xyz;
+	output.mpid2 = g_bufParticle[input.id].mpid2.xyz;
+
+	// load colour from Particle color
+	output.color = input.color;
+
+	return output;
 }
 
 //
@@ -116,48 +128,64 @@ VSParticleDrawOut VSParticleDraw(VSParticleIn input)
 void GSParticleDraw(point VSParticleDrawOut input[1], inout TriangleStream<GSParticleDrawOut> SpriteStream)
 {
 
-    GSParticleDrawOut output;
-    
-    for(int i=0; i<4; i++)
-    {
-        //highlight green particles
-        float green = input[0].color.g;
-        if((green-0.9f) > 0) green = 2.5f;
-        else green = 1.0f;
+	GSParticleDrawOut output;
 
-        float3 position = g_positions[i] * g_fParticleRad * green;
-        position = mul( position, (float3x3)g_mInvView ) + input[0].pos;
-        output.pos = mul( float4(position,1.0), g_mWorldViewProj ); 
-        output.color = input[0].color;        
-        output.tex = g_texcoords[i];
-        output.normal = normalize(mul(input[0].normal, (float3x3)g_mInvView));	//supposedly correct world normal
-        output.mpid1 = input[0].mpid1;
-        output.mpid2 = input[0].mpid2;
-        SpriteStream.Append(output);
-    }
-    SpriteStream.RestartStrip();
+	//calculate lighting data
+	float3 n = normalize(input[0].npos.xyz - input[0].pos.xyz);		// vertex normal
+	float3 l = normalize(lightpos.xyz);								// opposite of light direction (light: lightpos -> origo)
+
+	for (int i = 0; i<4; i++)
+	{
+		//highlight green particles
+		float green = input[0].color.g;
+		if ((green - 0.9f) > 0) green = 2.5f;
+		else green = 1.0f;
+
+		//vertex -> two triangles
+		float3 position = g_positions[i] * g_fParticleRad * green;
+		position = mul(position, (float3x3)g_mInvView) + input[0].pos;	// world position
+		output.pos = mul(float4(position, 1.0), g_mWorldViewProj);
+		
+		//shading
+		//original: output.color = input[0].color;
+		float3 v = normalize(eyepos.xyz - input[0].pos.xyz);
+		float3 h = normalize(v + l);
+		if (any(input[0].npos.xyz)){								// not null vector normal = model vertices
+			output.color = PhongBlinn(n, h, l);
+		}
+		else{														// null normal -> other vertices
+			output.color = input[0].color;
+		}
+
+		//texture and vertex IDs
+		output.tex = g_texcoords[i];
+		output.mpid1 = input[0].mpid1;
+		output.mpid2 = input[0].mpid2;
+		SpriteStream.Append(output);
+	}
+	SpriteStream.RestartStrip();
 }
 
 //
 // PS for drawing particles
 //
 float4 PSParticleDraw(PSParticleDrawIn input) : SV_Target
-{   
-    return g_txDiffuse.Sample( g_samLinear, input.tex ) * input.color;
+{
+	return g_txDiffuse.Sample(g_samLinear, input.tex) * input.color;
 }
 
 //
 // PS for drawing model (picking)
 //
 float4 PSModelDraw1(PSParticleDrawIn input) : SV_Target
-{   
-    return float4(input.mpid1,1.0f);
+{
+	return float4(input.mpid1, 1.0f);
 }
 
 //
 // PS for drawing model (picking)
 //
 float4 PSModelDraw2(PSParticleDrawIn input) : SV_Target
-{   
-    return float4(input.mpid2,1.0f);
+{
+	return float4(input.mpid2, 1.0f);
 }
--- Deformation/PosUpdate.hlsl
+++ Deformation/PosUpdate.hlsl
@@ -12,92 +12,112 @@
 
 struct Particle			// vertex in the input object
 {
-    float4 pos;
-    float4 normal;
-    float4 mpid1;
-    float4 mpid2;
+	float4 pos;
+	float4 npos;
+	float4 mpid1;
+	float4 mpid2;
 };
 
 struct Indexer
 {
-    float3 vc1index;	// index in first volumetric cube
-    float w1[8];		// neighbour weights in first volumetric cube
-    float3 vc2index;	// index in second volumetric cube
-    float w2[8];		// neighbour weights in second volumetric cube
+	float3 vc1index;	// index in first volumetric cube
+	float3 vc2index;	// index in second volumetric cube
+	float w1[8];		// neighbour weights in first volumetric cube
+	float w2[8];		// neighbour weights in second volumetric cube
+	float nw1[8];		// neighbour weights for normal
+	float nw2[8];		//			--||--
 };
 
 struct MassPoint		// vertex in the input volumetric cubes
 {
-    float4 oldpos;
-    float4 newpos;
-    float4 acc;
-    uint neighbour_same;
-    uint neighbour_other;
+	float4 oldpos;
+	float4 newpos;
+	float4 acc;
+	uint neighbour_same;
+	uint neighbour_other;
 };
 
-cbuffer cbCS : register( b0 )
+cbuffer cbCS : register(b0)
 {
-    uint vcwidth;			// number of masspoint in the smaller volcube in one row
-    uint vccell;			// size of one volcube cell
-    uint numparticles;		// total count of model vertices
-    
-    uint is_picking;		// bool for mouse picking
-    uint pickOriginX;		// windows height
-    uint pickOriginY;		// windows width
-    uint dummy1;			// dummy
-    uint dummy2;
-    float4 pickDir;			// picking vector direction
-    float4 eyePos;			// eye position
+	uint vcwidth;			// number of masspoint in the smaller volcube in one row
+	uint vccell;			// size of one volcube cell
+	uint numparticles;		// total count of model vertices
 
-    float stiffness;		// stiffness
-    float damping;			// damping, negative!
-    float dt;				// delta time
-    float im;				// inverse mass of masspoints
+	uint is_picking;		// bool for mouse picking
+	uint pickOriginX;		// windows height
+	uint pickOriginY;		// windows width
+	uint dummy1;			// dummy
+	uint dummy2;
+	float4 pickDir;			// picking vector direction
+	float4 eyePos;			// eye position
+
+	float stiffness;		// stiffness
+	float damping;			// damping, negative!
+	float dt;				// delta time
+	float im;				// inverse mass of masspoints
 };
 
-RWStructuredBuffer<Particle>  particles	: register( u0 );
-StructuredBuffer<Indexer>     indexer	: register( t0 );
-RWStructuredBuffer<MassPoint> volcube1	: register( u1 );
-RWStructuredBuffer<MassPoint> volcube2	: register( u2 );
+RWStructuredBuffer<Particle>  particles	: register(u0);
+StructuredBuffer<Indexer>     indexer	: register(t0);
+RWStructuredBuffer<MassPoint> volcube1	: register(u1);
+RWStructuredBuffer<MassPoint> volcube2	: register(u2);
 
 [numthreads(1, 1, 1)]
-void PosUpdate( uint3 DTid : SV_DispatchThreadID )
+void PosUpdate(uint3 DTid : SV_DispatchThreadID)
 {
-    // vertex# in DTid.x : 
-    //						0->numparticles = model vertices,
-    //						numparticles->numparticles+maxv1 = volcube1,
-    //						numparticles+maxv1->numparticles+maxv1+maxv2 = volcube2
-    
-    if(DTid.x < numparticles){
-        // Calculate new particle position
-        float3 oldppos = particles[DTid.x].pos.xyz;
-        int ind1 = indexer[DTid.x].vc1index.z*vcwidth*vcwidth + indexer[DTid.x].vc1index.y*vcwidth + indexer[DTid.x].vc1index.x;
-        int ind2 = indexer[DTid.x].vc2index.z*(vcwidth+1)*(vcwidth+1) + indexer[DTid.x].vc2index.y*(vcwidth+1) + indexer[DTid.x].vc2index.x;
-        float3 pos1 = indexer[DTid.x].w1[0] * volcube1[ind1].newpos.xyz + 
-                      indexer[DTid.x].w1[1] * volcube1[ind1+1].newpos.xyz + 
-                      indexer[DTid.x].w1[2] * volcube1[ind1+vcwidth].newpos.xyz + 
-                      indexer[DTid.x].w1[3] * volcube1[ind1+vcwidth+1].newpos.xyz + 
-                      indexer[DTid.x].w1[4] * volcube1[ind1+vcwidth*vcwidth].newpos.xyz + 
-                      indexer[DTid.x].w1[5] * volcube1[ind1+vcwidth*vcwidth+1].newpos.xyz + 
-                      indexer[DTid.x].w1[6] * volcube1[ind1+vcwidth*vcwidth+vcwidth].newpos.xyz + 
-                      indexer[DTid.x].w1[7] * volcube1[ind1+vcwidth*vcwidth+vcwidth+1].newpos.xyz;
-        float3 pos2 = indexer[DTid.x].w2[0] * volcube2[ind2].newpos.xyz + 
-                      indexer[DTid.x].w2[1] * volcube2[ind2+1].newpos.xyz + 
-                      indexer[DTid.x].w2[2] * volcube2[ind2+(vcwidth+1)].newpos.xyz + 
-                      indexer[DTid.x].w2[3] * volcube2[ind2+(vcwidth+1)+1].newpos.xyz + 
-                      indexer[DTid.x].w2[4] * volcube2[ind2+(vcwidth+1)*(vcwidth+1)].newpos.xyz + 
-                      indexer[DTid.x].w2[5] * volcube2[ind2+(vcwidth+1)*(vcwidth+1)+1].newpos.xyz + 
-                      indexer[DTid.x].w2[6] * volcube2[ind2+(vcwidth+1)*(vcwidth+1)+(vcwidth+1)].newpos.xyz + 
-                      indexer[DTid.x].w2[7] * volcube2[ind2+(vcwidth+1)*(vcwidth+1)+(vcwidth+1)+1].newpos.xyz;
-        // Set final position
-        particles[DTid.x].pos.xyz = pos1 * 0.5f + pos2 * 0.5f;
+	// vertex# in DTid.x : 
+	//						0->numparticles = model vertices,
+	//						numparticles->numparticles+maxv1 = volcube1,
+	//						numparticles+maxv1->numparticles+maxv1+maxv2 = volcube2
 
-        // Calculate new normals
-        float3 
-        
-    } else if(DTid.x >= numparticles && DTid.x < numparticles+maxv1){
-        particles[DTid.x].pos.xyz = volcube1[DTid.x-numparticles].newpos.xyz;
-    } else {
-        particles[DTid.x].pos.xyz = volcube2[DTid.x-numparticles-maxv1].newpos.xyz;
-    }
+	if (DTid.x < numparticles){
+		// Calculate new particle position
+		float3 oldppos = particles[DTid.x].pos.xyz;
+		int ind1 = indexer[DTid.x].vc1index.z*vcwidth*vcwidth + indexer[DTid.x].vc1index.y*vcwidth + indexer[DTid.x].vc1index.x;
+		int ind2 = indexer[DTid.x].vc2index.z*(vcwidth + 1)*(vcwidth + 1) + indexer[DTid.x].vc2index.y*(vcwidth + 1) + indexer[DTid.x].vc2index.x;
+		float3 pos1 = indexer[DTid.x].w1[0] * volcube1[ind1].newpos.xyz +
+			indexer[DTid.x].w1[1] * volcube1[ind1 + 1].newpos.xyz +
+			indexer[DTid.x].w1[2] * volcube1[ind1 + vcwidth].newpos.xyz +
+			indexer[DTid.x].w1[3] * volcube1[ind1 + vcwidth + 1].newpos.xyz +
+			indexer[DTid.x].w1[4] * volcube1[ind1 + vcwidth*vcwidth].newpos.xyz +
+			indexer[DTid.x].w1[5] * volcube1[ind1 + vcwidth*vcwidth + 1].newpos.xyz +
+			indexer[DTid.x].w1[6] * volcube1[ind1 + vcwidth*vcwidth + vcwidth].newpos.xyz +
+			indexer[DTid.x].w1[7] * volcube1[ind1 + vcwidth*vcwidth + vcwidth + 1].newpos.xyz;
+		float3 pos2 = indexer[DTid.x].w2[0] * volcube2[ind2].newpos.xyz +
+			indexer[DTid.x].w2[1] * volcube2[ind2 + 1].newpos.xyz +
+			indexer[DTid.x].w2[2] * volcube2[ind2 + (vcwidth + 1)].newpos.xyz +
+			indexer[DTid.x].w2[3] * volcube2[ind2 + (vcwidth + 1) + 1].newpos.xyz +
+			indexer[DTid.x].w2[4] * volcube2[ind2 + (vcwidth + 1)*(vcwidth + 1)].newpos.xyz +
+			indexer[DTid.x].w2[5] * volcube2[ind2 + (vcwidth + 1)*(vcwidth + 1) + 1].newpos.xyz +
+			indexer[DTid.x].w2[6] * volcube2[ind2 + (vcwidth + 1)*(vcwidth + 1) + (vcwidth + 1)].newpos.xyz +
+			indexer[DTid.x].w2[7] * volcube2[ind2 + (vcwidth + 1)*(vcwidth + 1) + (vcwidth + 1) + 1].newpos.xyz;
+		// Set final position
+		particles[DTid.x].pos.xyz = pos1 * 0.5f + pos2 * 0.5f;
+
+		// Calculate new normals
+		float3 npos1 = indexer[DTid.x].nw1[0] * volcube1[ind1].newpos.xyz +
+			indexer[DTid.x].nw1[1] * volcube1[ind1 + 1].newpos.xyz +
+			indexer[DTid.x].nw1[2] * volcube1[ind1 + vcwidth].newpos.xyz +
+			indexer[DTid.x].nw1[3] * volcube1[ind1 + vcwidth + 1].newpos.xyz +
+			indexer[DTid.x].nw1[4] * volcube1[ind1 + vcwidth*vcwidth].newpos.xyz +
+			indexer[DTid.x].nw1[5] * volcube1[ind1 + vcwidth*vcwidth + 1].newpos.xyz +
+			indexer[DTid.x].nw1[6] * volcube1[ind1 + vcwidth*vcwidth + vcwidth].newpos.xyz +
+			indexer[DTid.x].nw1[7] * volcube1[ind1 + vcwidth*vcwidth + vcwidth + 1].newpos.xyz;
+		float3 npos2 = indexer[DTid.x].nw2[0] * volcube2[ind2].newpos.xyz +
+			indexer[DTid.x].nw2[1] * volcube2[ind2 + 1].newpos.xyz +
+			indexer[DTid.x].nw2[2] * volcube2[ind2 + (vcwidth + 1)].newpos.xyz +
+			indexer[DTid.x].nw2[3] * volcube2[ind2 + (vcwidth + 1) + 1].newpos.xyz +
+			indexer[DTid.x].nw2[4] * volcube2[ind2 + (vcwidth + 1)*(vcwidth + 1)].newpos.xyz +
+			indexer[DTid.x].nw2[5] * volcube2[ind2 + (vcwidth + 1)*(vcwidth + 1) + 1].newpos.xyz +
+			indexer[DTid.x].nw2[6] * volcube2[ind2 + (vcwidth + 1)*(vcwidth + 1) + (vcwidth + 1)].newpos.xyz +
+			indexer[DTid.x].nw2[7] * volcube2[ind2 + (vcwidth + 1)*(vcwidth + 1) + (vcwidth + 1) + 1].newpos.xyz;
+		particles[DTid.x].npos.xyz = npos1 * 0.5f + npos2 * 0.5f;
+		
+	}
+	else if (DTid.x >= numparticles && DTid.x < numparticles + maxv1){
+		particles[DTid.x].pos.xyz = volcube1[DTid.x - numparticles].newpos.xyz;
+	}
+	else {
+		particles[DTid.x].pos.xyz = volcube2[DTid.x - numparticles - maxv1].newpos.xyz;
+	}
 }
